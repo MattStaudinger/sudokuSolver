6d93b9c461288f0befc0857575e9ce48d926257a
Matthias Staudinger <m.staudinger@supervista.de>
2020-03-02 22:51:56 +0100

    add box and column validation

--- src/Logic/Validator.js
+++ src/Logic/Validator.js
@@ -3,9 +3,24 @@
 class Validator {
 
   validate = sudokuBoard => {
-    let {isValid, positionOfInvalidNumber} = this.validateRows(sudokuBoard)
 
-    return {isValid, positionOfInvalidNumber};
+    let validRows = this.validateRows(sudokuBoard)
+    if (!validRows.isValid) 
+      return {isValid: false, positionOfInvalidNumber: validRows.positionOfInvalidNumber};
+
+    let validColumns = this.validateColumns(sudokuBoard)
+    if (!validColumns.isValid) 
+      return {isValid: false, positionOfInvalidNumber: validColumns.positionOfInvalidNumber};
+
+    let validBoxes = this.validateBoxes(sudokuBoard)
+    if (!validBoxes.isValid) 
+      return {isValid: false, positionOfInvalidNumber: validBoxes.positionOfInvalidNumber};
+      
+    
+    return {isValid: true, positionOfInvalidNumber: null};
+
+
+
   };
 
 
@@ -13,7 +28,7 @@ class Validator {
 
     for (let y = 0; y < 9; y++) {
       const row = sudokuBoard[y];
-      let {isValid, positionOfInvalidNumber} = this.validateSingleRow(row)      
+      let {isValid, positionOfInvalidNumber} = this.validate1DArray(row)      
       if (!isValid) 
         return {isValid, positionOfInvalidNumber: `${positionOfInvalidNumber+1}-${y+1}`};
     }
@@ -29,23 +44,97 @@ class Validator {
   }
 
 
-  validateSingleRow = (row) => {
+  validate1DArray = (array) => {
 
-    for (let x = 0; x < 9; x++) {
-      const numberToSearchInRow = row[x];
-      if (!numberToSearchInRow) continue; //if number is 0, then don't compare it
-      if (this.isNumberMoreThanOnceInArray(numberToSearchInRow, row)) 
-        return {isValid: false, positionOfInvalidNumber: x}      
+    for (let pos = 0; pos < 9; pos++) {
+      const numberToSearch = array[pos];
+      if (!numberToSearch) continue; //if number is 0, then don't compare it
+      if (this.isNumberMoreThanOnceInArray(numberToSearch, array)) 
+        return {isValid: false, positionOfInvalidNumber: pos}      
     }
 
     return {isValid: true, positionOfInvalidNumber: null}
    
   };
 
+  switchXandYOfBoard = sudokuBoard => {
+    let sudokuBoardSwitchedXAndY = [];
 
-  validateColumn = (field, sudokuBoard) => {};
+    for (let x = 0; x < 9; x++) {
+      sudokuBoardSwitchedXAndY[x] = []
+      for (let y = 0; y < 9; y++) {       
+        sudokuBoardSwitchedXAndY[x][y] = sudokuBoard[y][x]
+      }
+    }
+    return sudokuBoardSwitchedXAndY;
 
-  validateBox = (field, sudokuBoard) => {};
+  }
+
+
+  validateColumns = (sudokuBoard) => {
+
+    let sudokuBoardSwitchedXAndY = this.switchXandYOfBoard(sudokuBoard)
+
+    for (let x = 0; x < 9; x++) {
+      const column = sudokuBoardSwitchedXAndY[x];
+      let {isValid, positionOfInvalidNumber} = this.validate1DArray(column)      
+      if (!isValid) 
+        return {isValid, positionOfInvalidNumber: `${x+1}-${positionOfInvalidNumber+1}`};
+    }
+
+    return {isValid: true, positionOfInvalidNumber: null};
+
+  };
+
+  translateBoardToBoxArrays = (sudokuBoard) => {
+
+    let sudokuBoardArrayBoxes = []
+
+    for (let z = 0; z<9; z++) {
+      sudokuBoardArrayBoxes[z] = []
+    }
+    
+    for (let y = 0; y < 9; y++) {
+      for (let x = 0; x < 9; x++) { 
+     
+        if (x < 3 && y < 3)
+          sudokuBoardArrayBoxes[0].push(sudokuBoard[y][x])        
+        else if (x >= 3 && x < 6 && y < 3) 
+          sudokuBoardArrayBoxes[1].push(sudokuBoard[y][x])        
+        else if (x >= 6 && y < 3)
+          sudokuBoardArrayBoxes[2].push(sudokuBoard[y][x])
+        else if (x < 3 && y >=3 && y < 6)
+          sudokuBoardArrayBoxes[3].push(sudokuBoard[y][x])
+        else if (x >=3 && x< 6 && y >=3 && y < 6)
+          sudokuBoardArrayBoxes[4].push(sudokuBoard[y][x])
+        else if (x >= 6 && y >=3 && y < 6)
+          sudokuBoardArrayBoxes[5].push(sudokuBoard[y][x])
+        else if (x < 3 && y >= 6)
+          sudokuBoardArrayBoxes[6].push(sudokuBoard[y][x])
+        else if (x >=3 && x< 6 && y >= 6)
+          sudokuBoardArrayBoxes[7].push(sudokuBoard[y][x])
+        else if (x >= 6 && y >= 6)
+          sudokuBoardArrayBoxes[8].push(sudokuBoard[y][x])  
+      }
+    }
+    return sudokuBoardArrayBoxes;
+  }
+
+
+  validateBoxes = (sudokuBoard) => {
+
+    let sudokuBoardBoxArrays = this.translateBoardToBoxArrays(sudokuBoard)
+
+    for (let x = 0; x < 9; x++) {
+      const box = sudokuBoardBoxArrays[x];
+      let {isValid, positionOfInvalidNumber} = this.validate1DArray(box)
+      if (!isValid) 
+        return {isValid, positionOfInvalidNumber: `${positionOfInvalidNumber+1}-${x}`};
+    }
+
+    return {isValid: true, positionOfInvalidNumber: null};
+
+  };
 
 }
 
